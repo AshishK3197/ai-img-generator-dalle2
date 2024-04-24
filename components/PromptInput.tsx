a1aa777ac18458b2e2921cc53c8145266772c888
Ashish Kumar <ashishkumar@Ashishs-Laptop.local>
2024-04-24 22:14:41 +0530

    Setting up backend

--- components/PromptInput.tsx
+++ components/PromptInput.tsx
@@ -7,7 +7,7 @@ import useSWR from "swr";
 function PromptInput() {
   const [input, setInput] = useState("");
 
-  const {data : suggestion , error , isLoading} = useSWR('/api/suggestion' , fetchSuggestionFromChatGPT , {
+  const {data : suggestion , isLoading , mutate , isValidating} = useSWR('/api/suggestion' , fetchSuggestionFromChatGPT , {
     revalidateOnFocus : false,
   });
 
